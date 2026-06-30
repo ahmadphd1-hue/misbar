68c4c30e5d0fc56cd052f41a77930b058e5081ab
Alshamlani <ahmadphd1@gmail.com>
2026-06-30 21:32:38 +0800

    fix: import structureTool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,13 @@
 'use client'
 
 /**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
+ * This configuration is used for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
  */
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+// قمنا بتغيير هذا السطر:
+import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
